06de1248d013f9591e56e6383cd93f0de1293dbf
subrat0018 <subratchandra2003@gmail.com>
2024-06-28 21:46:00 +0530

    feat: TMDB API integrated

--- public/background.js
+++ public/background.js
@@ -2,10 +2,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url?.includes("google.com/search")) {
       chrome.tabs.sendMessage(tabId, { message: "parse_movie_title" }, (response) => {
         if (response?.movieName) {
-            console.log(response.movieName)
+            fetchMovieDetails(response.movieName);
         }
       });
     }
   });
   
-  
+  function fetchMovieDetails(movieName) {
+    const apiKey = "efd6f2d9596f0bbf7b92956d9ef2d643";
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieName}`)
+      .then(response => response.json())
+      .then(data => {
+        const movie = data.results[0];
+        if (movie) {
+          chrome.storage.local.set({ [movieName]: movie });
+          console.log(movie);
+        }
+      });
+  }
