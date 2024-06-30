ebcde4109adb0fcb9941db9522fbbbe8b937e9f9
subrat0018 <subratchandra2003@gmail.com>
2024-06-30 15:57:14 +0530

    fix: enhanced the search result parsing

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,12 +1,25 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "parse_movie_title") {
-    let movieName = document.getElementsByClassName("B5dxMb")[0].innerHTML;
-    console.log(movieName);
-    if(movieName){
-      sendResponse({data: movieName });
-    }else{
-      sendResponse({error: "No movie found"});
-    }
+      let movieName = null;
+  
+      const selectors = [
+        ".B5dxMb",      //Google suggested
+        ".DKV0Md",      //Wikipedia     
+        ".MBeuO",       //IMDB and general search results heading                   
+      ];
+  
+      for (let selector of selectors) {
+        const element = document.querySelector(selector);
+        if (element && element.innerText) {
+          movieName = element.innerText.split(" ")[0];
+          break;
+        }
+      }
+      if (movieName) {
+        sendResponse({ data: movieName });
+      } else {
+        sendResponse({ error: "No movie found" });
+      }
     }
   });
   
